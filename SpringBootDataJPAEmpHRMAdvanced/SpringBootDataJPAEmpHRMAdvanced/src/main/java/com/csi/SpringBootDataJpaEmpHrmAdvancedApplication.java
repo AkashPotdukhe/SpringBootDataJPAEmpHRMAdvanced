30c7b1e605d23c8cdc3fb2bf1d836e7283370303
AkashPotdukhe <ampotdukhe25@gmail.com>
2022-12-15 18:18:42 +0530

    Initial Commit for Employee Management Application

---
+++ SpringBootDataJPAEmpHRMAdvanced/SpringBootDataJPAEmpHRMAdvanced/src/main/java/com/csi/SpringBootDataJpaEmpHrmAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaEmpHrmAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaEmpHrmAdvancedApplication.class, args);
+	}
+
+}
